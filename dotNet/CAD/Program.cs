34d053da04b7b76520ded69c3fc49da41673c8a3
JulienHenrotin <julien.henrotin@viacesi.fr>
2020-06-23 22:18:55 +0200

    connexion BDD

--- dotNet/CAD/Program.cs
+++ dotNet/CAD/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;  
+using Microsoft.SqlServer.Management.Common; 
 
 namespace CAD
 {
@@ -10,7 +12,24 @@ namespace CAD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("azdiauzdhuoazhdiiahzdpi");
+            Console.WriteLine("Hello world CAD !!");
+            
+             String sqlServerLogin = "user_id";  
+             String password = "pwd";  
+             String instanceName = "DESKTOP-SOSI265";  
+             String remoteSvrName = "DESKTOP-SOSI265";  
+            
+           
+            Server srv1 = new Server();   
+            srv1.ConnectionContext.LoginSecure = true;   
+            srv1.ConnectionContext.Login = sqlServerLogin;  
+            srv1.ConnectionContext.Password = password;  
+            Console.WriteLine(srv1.Information.Version);
+            
+            
+            
+             
+            Console.Read();
         }
     }
 }
